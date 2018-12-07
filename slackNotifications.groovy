c884a669b728acccea9e01fb7b0a12d3569c4ccb
ricardo-softinsa <45635917+ricardo-softinsa@users.noreply.github.com>
2018-12-07 12:11:59 +0000

    Update slackNotifications.groovy

--- slackNotifications.groovy
+++ slackNotifications.groovy
@@ -1,27 +1,27 @@
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful!"
+    slackSend color: "good", message: "Job: ${env.JOB_NAME} Build: # ${env.BUILD_NUMBER} was successful!"
   }
   else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} Build: # ${env.BUILD_NUMBER} was failed"
   }
   else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
+    slackSend color: "warning", message: "Job: ${env.JOB_NAME} Build: # ${env.BUILD_NUMBER} was unstable"
   }
   else if( buildResult == "ABORTED" ) { 
-    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was aborted..."
+    slackSend color: "warning", message: "Job: ${env.JOB_NAME} Build: # ${env.BUILD_NUMBER} was aborted..."
   }
   else {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} result was unclear..."	
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} Build: # ${env.BUILD_NUMBER} result was unclear..."	
   }
 }
 
 def afterQG(String QAResult) {
   if(QAResult == "OK"){
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} passed SonarQube Quality Gates!"   
+    slackSend color: "good", message: "Job: ${env.JOB_NAME} Build: # ${env.BUILD_NUMBER} passed SonarQube Quality Gates!"   
   }
   else if(QAResult == "ERROR") { 
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} didn't pass SonarQube Quality Gates!"
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} Build: # ${env.BUILD_NUMBER} didn't pass SonarQube Quality Gates!"
   }
 }
 
